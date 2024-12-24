587c099e103e92de42063a74a5c706361b3e6e17
caydenw <caydenw@netflix.com>
2024-12-23 16:58:21 -0800

    finish lecture 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,40 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["👻", "🎃", "🍭", "🕷️"]
+    let emojis = ["👻","🎃","🍭","🕷️","👻","🎃","🍭","🕷️","👻","🎃","🍭","🕷️","👻","🎃","🍭","🕷️"]
+    
+    @State var cardCount = 4
     
     var body: some View {
-        HStack {
-            ForEach(emojis.indices, id: \.self) { index in
+        VStack {
+            ScrollView {
+                cards
+            }
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    
+    struct CardView: View {
+        @State var isFaceUp = true
+        let content: String
+        
+        var body: some View {
+            ZStack {
+                let base = RoundedRectangle(cornerRadius: 12)
+                
+                Group {
+                    base.foregroundColor(.white)
+                    base.strokeBorder(lineWidth: 2)
+                    Text(content).font(.largeTitle)
+                }
+                .opacity(isFaceUp ? 1 : 0)
+                base.fill().opacity(isFaceUp ? 0 : 1)
+            }
+            .onTapGesture {
+                isFaceUp.toggle()
+            }
+        }
+    }
+    
+    var cards: some View {
+        LazyVGrid (columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(00..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
-        .padding()
-        
     }
-}
-
-struct CardView: View {
-    @State var isFaceUp = true
-    let content: String
     
-    var body: some View {
-        ZStack {
-            let base = RoundedRectangle(cornerRadius: 12)
-            
-            if (isFaceUp) {
-                base.foregroundColor(.white)
-                base.strokeBorder(lineWidth: 2)
-                Text(content).font(.largeTitle)
+    func cardCountAdjuster(by offset: Int, icon: String, label: String? = nil) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: icon)
+            if let label = label {
+                Text(label)
             }
-            else {
-                base.fill()
-            }
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardCountAdjuster(by: -1, icon: "minus.circle", label: "Remove Card")
+            Spacer()
+            cardCountAdjuster(by: 1, icon: "plus.circle", label: "Add Card")
         }
-        .onTapGesture {
-            isFaceUp.toggle()
-        }
+        .imageScale(.large)
+        .font(.title3)
     }
 }
 
