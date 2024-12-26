05acc54aaddea9471a541ad1e637aa7fff6b312b
caydenw <caydenw@netflix.com>
2024-12-26 14:20:05 -0800

    continue developing MVVM architecture

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,38 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Cayden Wagner on 12/25/24.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame: ObservableObject {
+    private static let emojis = ["👻","🎃","🍭","🕷️", "💀","😱","👽","🧔‍♀️"]
+    
+    private static func createMemoryGame() -> MemoryGame<String> {
+        return MemoryGame(numberOfPairsOfCards: 20) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+            } else {
+                return "❌"
+            }
+        }
+    }
+        
+    @Published private var model = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+    // MARK: - Intents
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
+    }
+    
+    func shuffle() {
+        model.shuffle()
+    }
+}
