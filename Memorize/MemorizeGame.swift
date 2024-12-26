e2c18e230943627c44cfb46af0b63fe90263e93b
caydenw <caydenw@netflix.com>
2024-12-25 22:50:27 -0800

    create model and view model.

--- Memorize/MemorizeGame.swift
+++ Memorize/MemorizeGame.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct MemorizeGame<CardContent> {
+    var Cards: Array<Card>
+    
+    func choose(card: Card) {
+        
+    }
+    
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+    }
+}
