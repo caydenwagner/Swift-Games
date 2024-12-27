64aac7df72e607847e66d8b1a7309bb87d8aef40
caydenw <caydenw@netflix.com>
2024-12-27 14:40:21 -0800

    Implement game logic in the model

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
     private(set) var cards: Array<Card>
     
     init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
@@ -21,17 +21,42 @@ struct MemoryGame<CardContent> {
         }
     }
     
-    func choose(_ card: Card) {
-        
+    var indexOfTheOneAndOnlyFaceUpCard: Int? {
+        get { cards.indices.filter({ index in cards[index].isFaceUp }).only }
+        set { cards.indices.forEach({ cards[$0].isFaceUp = ($0 == newValue) ? true : false }) }
+    }
+    
+    mutating func choose(_ card: Card) {
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }) {
+            if !cards[chosenIndex].isFaceUp && !cards[chosenIndex].isMatched {
+                if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+                    if cards[potentialMatchIndex].content == cards[chosenIndex].content {
+                        cards[potentialMatchIndex].isMatched = true
+                        cards[chosenIndex].isMatched = true
+                    }
+                } else {
+                    indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+                }
+                cards[chosenIndex].isFaceUp = true
+            }
+        }
     }
     
     mutating func shuffle() {
         cards.shuffle()
     }
     
-    struct Card {
-        var isFaceUp = true
+    struct Card: Equatable, Identifiable {
+        var isFaceUp = false
         var isMatched = false
         let content: CardContent
+        
+        var id: UUID = UUID()
+    }
+}
+
+extension Array {
+    var only: Element? {
+         count == 1 ? first : nil
     }
 }
