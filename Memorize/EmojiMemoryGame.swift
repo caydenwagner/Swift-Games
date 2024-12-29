44caea94ffb612cee826decd91653efe4caec698
caydenw <caydenw@netflix.com>
2024-12-28 21:54:34 -0800

    Separate AspectVGrid into a separate file.

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -11,7 +11,7 @@ class EmojiMemoryGame: ObservableObject {
     private static let emojis = ["👻","🎃","🍭","🕷️", "💀","😱","👽","🧔‍♀️"]
     
     private static func createMemoryGame() -> MemoryGame<String> {
-        return MemoryGame(numberOfPairsOfCards: 8) { pairIndex in
+        return MemoryGame(numberOfPairsOfCards: 12) { pairIndex in
             if emojis.indices.contains(pairIndex) {
                 return emojis[pairIndex]
             } else {
