e9831a753e6bd8969d677b043e32e365bddba0a1
caydenw <caydenw@netflix.com>
2024-12-29 17:09:49 -0800

    init set project

---
+++ Set/Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Cayden Wagner on 12/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Set/SetTests/SetTests.swift
@@ -0,0 +1,17 @@
+//
+//  SetTests.swift
+//  SetTests
+//
+//  Created by Cayden Wagner on 12/29/24.
+//
+
+import Testing
+@testable import Set
+
+struct SetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
